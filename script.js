fa91538539297cd7ebaf7ff736ed24b786cb12b8
LootNex <96824976+LootNex@users.noreply.github.com>
2023-05-29 20:33:52 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    
-    let button = document.getElementById(`header_burger`)
-    let button_2 = document.getElementById(`header__menu`)
-        
-    button.addEventListener('click', () => {
-        button_2.classList.toggle("active")
-    });
-}
